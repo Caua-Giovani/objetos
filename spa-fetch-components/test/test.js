6480c5b42c0ee0b37f93f498bb20ce8ebec72ca0
Cauã Giovani <caua.g.andrade7@aluno.senai.br>
2026-04-24 15:00:26 -0300

    modify: testes

--- spa-fetch-components/test/test.js
+++ spa-fetch-components/test/test.js
@@ -1,16 +1,49 @@
-try{
-    let x=y
-    let y=x
+// let result = "Not Active.";
+// let isActive = false;
 
-} catch(erro){
+// console.log(isActive,result)
 
-    let text = `
-    <strong>Nome do erro:</strong> ${erro.name} <br />
-    <strong>Mensagem:</strong> ${erro.message} <br />
-    <strong>Stack:</strong> <span>${erro.stack}</span>
-    `;
+// // ❌ Assignment, not comparison
+// if (isActive === true) {
+//     result = "Active!";
+// }else{
+//     result = "Not Active."
+// }
 
-    document.body.innerHTML = text
+// console.log(isActive,result)
 
+// try{
+    
+
+// } catch(erro){
+
+//     let text = `
+//     <strong>Nome do erro:</strong> ${erro.name} <br />
+//     <strong>Mensagem:</strong> ${erro.message} <br />
+//     <strong>Stack:</strong> <span>${erro.stack}</span>
+//     `;
+
+//     document.body.innerHTML = text
+
+// };
+
+const container=document.querySelector('.container')
+
+const carregarComponente = async (caminho,container) => {
+    try{
+        const resposta = await fetch(caminho);
+
+        if (!resposta.ok) {
+            throw new Error('Erro ao carregar componente')
+        }
+
+        const html = await resposta.text();
+
+        container.innerHTML = html;
+        
+    }catch(erro){
+        console.error(erro)
+    }
 };
 
+carregarComponente('./c-test.html',container)
