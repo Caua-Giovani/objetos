18828963ba899162ab26ffe98ee34453a220eb02
Cauã Giovani <caua.g.andrade7@aluno.senai.br>
2026-04-30 09:57:04 -0300

    modify: fazendo o js do test

--- spa-fetch-components/test/test.js
+++ spa-fetch-components/test/test.js
@@ -1,49 +1,62 @@
-// let result = "Not Active.";
-// let isActive = false;
+/* Testes com os exemplos da w3 Schools */
 
-// console.log(isActive,result)
+/* Lançar um excessão (erro personalizado) */
 
-// // ❌ Assignment, not comparison
-// if (isActive === true) {
-//     result = "Active!";
-// }else{
-//     result = "Not Active."
-// }
+try{
 
-// console.log(isActive,result)
+    let idade = 2;
 
-// try{
-    
-
-// } catch(erro){
-
-//     let text = `
-//     <strong>Nome do erro:</strong> ${erro.name} <br />
-//     <strong>Mensagem:</strong> ${erro.message} <br />
-//     <strong>Stack:</strong> <span>${erro.stack}</span>
-//     `;
-
-//     document.body.innerHTML = text
-
-// };
-
-const container=document.querySelector('.container')
-
-const carregarComponente = async (caminho,container) => {
-    try{
-        const resposta = await fetch(caminho);
-
-        if (!resposta.ok) {
-            throw new Error('Erro ao carregar componente')
-        }
-
-        const html = await resposta.text();
-
-        container.innerHTML = html;
-        
-    }catch(erro){
-        console.error(erro)
+    if (idade < 0){
+        throw GeradorErros('Idade Invalida',1001,'VALIDACAO')
     }
+
+} catch(erro){
+
+    let text = `
+    <strong>Nome do erro:</strong> ${erro.name} <br />
+    <strong>Mensagem:</strong> ${erro.message} <br />
+    <strong>Código</strong> ${erro.codigo}<br />
+    <strong>Tipo</strong> ${erro.tipo}<br />
+    <strong>Stack:</strong> <span>${erro.stack}</span>
+    `;
+
+    document.body.innerHTML = text
+
 };
 
-carregarComponente('./c-test.html',container)
+
+/* Criar uma função geradora de erro */
+
+function GeradorErros(mensagem, codigo, tipo){
+    let erro = new Error(mensagem)
+    erro.codigo = codigo
+    erro.tipo = tipo
+
+    return erro
+}
+
+try{
+
+    let saldo = 200;
+
+    if (saldo < 200){
+        throw GeradorErros('Saldo Insuficiente',1100,'VALIDACAO')
+    }
+
+} catch(erro){
+
+    console.log(erro)
+
+};
+
+/* Testando a função: carregarComponente
+01 - Criar o componente que será injetado
+02 - Criar o elemento que irá receber o componente (no index index.html) (<div id="app"></div>)
+03 - Capturar o elemento que irá receber o componente( no index.html)
+04 - Utilizar a função para carregar o componente */
+
+
+/* Criar uma requisição HTTP com fetch e .then */
+
+/* Criar uma requisição HTTP com fecth e async/await */
+
